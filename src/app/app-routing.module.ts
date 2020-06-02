7afa9d23045a504cc220fdc7de8eb73db147d21c
Kunju Perath <kperath@gmail.com>
2020-06-02 19:46:59 -0400

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import {DashboardComponent} from './dashboard/dashboard.component';
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'tile', component: NasaPodComponent }
+  { path: 'dashboard/tile', component: NasaPodComponent }
 ];
 
 @NgModule({

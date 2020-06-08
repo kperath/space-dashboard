16e5f38260ba8c8cbf5303e465640fb128c9497c
Kunju Perath <kperath@gmail.com>
2020-06-08 14:13:44 -0400

    successfully created multiple tiles woooooo

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,13 +7,56 @@ import { Component, OnInit } from "@angular/core";
 })
 export class DashboardComponent implements OnInit {
 
-  nasaTileImageURL = "/assets/images/apod.jpg";
-  nasaTileDescription = "See Nasa's Astronomy Picture of the Day";
-  nasaImageDate = "";
+  // nasaTileImageURL1 = "/assets/images/apod.jpg";
+  // nasaTileDescription1 = "See Nasa's Astronomy Picture of the Day";
+  // nasaImageDate1 = "";
+  //
+  // nasaTileImageURL2 = "/assets/images/apod.jpg";
+  // nasaTileDescription2 = "New Yearrrrrrr";
+  // nasaImageDate2 = "2020-01-01";
 
-  nasaTileImageURL2 = "/assets/images/apod.jpg";
-  nasaTileDescription2 = "New Yearrrrrrr";
-  nasaImageDate2 = "2020-01-01";
+  nasaTileArray = [
+    {
+      nasaTileImageURL: "/assets/images/apod.jpg",
+      nasaTileDescription: "See Nasa's Astronomy Picture of the Day",
+      nasaImageDate: ""
+    },
+    {
+      nasaTileImageURL: "/assets/images/apod.jpg",
+      nasaTileDescription: "New Year 2020 Astronomy Picture!",
+      nasaImageDate: "2020-01-01"
+    },
+    {
+      nasaTileImageURL: "/assets/images/apod.jpg",
+      nasaTileDescription: "New Year 2019 Astronomy Picture!",
+      nasaImageDate: "2019-01-01"
+    },
+    {
+      nasaTileImageURL: "/assets/images/apod.jpg",
+      nasaTileDescription: "New Year 2018 Astronomy Picture!",
+      nasaImageDate: "2018-01-01"
+    },
+    {
+      nasaTileImageURL: "/assets/images/apod.jpg",
+      nasaTileDescription: "New Year 2017 Astronomy Picture!",
+      nasaImageDate: "2017-01-01"
+    },
+    {
+      nasaTileImageURL: "/assets/images/apod.jpg",
+      nasaTileDescription: "New Year 2016 Astronomy Picture!",
+      nasaImageDate: "2016-01-01"
+    },
+    {
+      nasaTileImageURL: "/assets/images/apod.jpg",
+      nasaTileDescription: "New Year 2015 Astronomy Picture!",
+      nasaImageDate: "2015-01-01"
+    },
+    {
+      nasaTileImageURL: "/assets/images/apod.jpg",
+      nasaTileDescription: "New Year 2014 Astronomy Picture!",
+      nasaImageDate: "2014-01-01"
+    }
+  ];
 
   constructor() { }
 
